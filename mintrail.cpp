dd1163c7a5d425c55f7c5d803a1e79ab3158705b
varunkamath <varun.kamath@gmail.com>
2015-05-14 10:56:03 -0500

    kinda merged

--- mintrail.cpp
+++ mintrail.cpp
@@ -17,7 +17,34 @@ int main()
 	cout << "|    )(  ) __ ( ) _)   / \\/ \\ )( /    //    / ) _) \\___ \\(  O ) )( /    \\    )(   )   //    \\ )( / (_/\\  |" << endl;
 	cout << "|   (__) \\_)(_/(____)  \\_)(_/(__)\\_)__)\\_)__)(____)(____/ \\__/ (__)\\_/\\_/   (__) (__\\_)\\_/\\_/(__)\\____/  |" << endl;
 	cout << "+--------------------------------------------------------------------------------------------------------+" << endl;
-	cout << "\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f\f";
-	cout << "test\n";
+	cout << "\f\f\f\f\f";
+	char choice = ' ';
+	int wagon = 0;
+    
+    cout << "You are about to embark on the Minnesota Trail; a path to fulfilling your destiny" <<endl;
+	cout << "Want to continue on this eventful journey?";
+	cout << "a. Yes";
+	cout << "b. No";
+	cin >> choice;
+
+	if (choice == 'a') {
+		cout << "Let us begin the adventure" <<endl;
+	}
+	if (choice == 'b') {
+		cout << "Weak. Too bad, Everything of value was stolen from you."; 
+		cout << "You have no choice but to continue" <<endl;
+	}
+
+	cout << "Luckily, you have enough, $1000, to buy a wagon and supplies" <<endl;
+	cout << "Here are your options (Both wagons cost $500)";
+	cout << "1. The wagon has enhanced storage space for food, water, and ammunition, but is susceptible to damage" <<endl;
+	cout << "2. This wagon enables one to camp within the wagon and build a fire, but has less storage space" <<endl;
+	
+	if (wagon == 1) {
+		cout << "Wise choice, but let's hope the wagon holds together.. " <<endl;
+	}
+	if (wagon == 2) {
+		cout << "The comfort wagon it is. But maybe no food for you..." <<endl;
+	}
 	return 0;
 }
