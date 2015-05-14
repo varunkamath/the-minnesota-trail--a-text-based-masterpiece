afaf1dff53c9df2daee148377041b898c0dddf58
varunkamath <varun.kamath@gmail.com>
2015-05-14 10:17:30 -0500

    inception

---
+++ mintrail.cpp
@@ -0,0 +1,16 @@
+//title: the minnesota trail
+//publishers: v/arun
+//Description: slightly less inefficient calculator
+//Date: 10 May 2015
+
+#include <iostream>
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+	
+	return 0;
+}
